da13602b0338fc783565bfe7f2482cf39df7927a
Danila <Danila99Chernobaew@mail.ru>
2024-04-29 10:54:12 +0200

    написал часть, отвечающую за создание и заполнение массива

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+    Console.Write("Введите количество элементов массива:\t");
+
+    int elementCount = int.Parse(Console.ReadLine());
+
+
+    string[] myArray = new string[elementCount];
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write($"Введите элемент массива пол индексом {i}:\t ");
+        myArray[i] = Console.ReadLine();
+        
+    }
+    Console.WriteLine("Вывод массива");
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.WriteLine(myArray[i]);
+    }
+
+    Console. Write(myArray[1].Length);
