91c1bad60abcd0494aed144413501e1a74064bd3
Danila <Danila99Chernobaew@mail.ru>
2024-06-23 11:57:59 +0200

    "исправление неточностей"

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿
-string[] CreateArr(int elementCount) {
+string[] CreateArr(int elementCount) {          //Создаём массив и заносим туда значения
 
     string[] myArray = new string[elementCount];
     
 
     for (int i = 0; i < myArray.Length; i++)
     {
-        Console.Write($"Введите элемент массива пол индексом {i}:\t ");
+        Console.Write($"Введите элемент массива под индексом {i}:\t ");
         myArray[i] = Console.ReadLine();
         
     }
@@ -15,21 +15,21 @@ string[] CreateArr(int elementCount) {
    
 }
 
- string[] ArrSelection (string[] myArray) {
+ string[] ArrSelection (string[] myArray) {         //создаём второй массив и сразу сортируем в него значения первого
         string[] SelectArray = new string[myArray.Length];
-        int k =0;
+        int SecondArrayCount =0;
      for (int i = 0; i < myArray.Length; i++)
      {
         if (myArray[i].Length<= 3)
         {
-            SelectArray[k] = myArray[i];
-            k++;
+            SelectArray[SecondArrayCount] = myArray[i];
+            SecondArrayCount++;
         }
      }
      return SelectArray;
 }
 
-void Conclusion() 
+void Conclusion()                                     //Выводим всё вышенаписанное
 {
     Console.Write("Введите количество элементов массива:\t");
     int elementCount = int.Parse(Console.ReadLine());
